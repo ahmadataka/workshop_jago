63771a13837824e2c42130d11c9822eb8669bc8d
Ahmad Ataka <ataka.ahmad@gmail.com>
2023-03-18 08:31:21 +0700

    Modify testing

--- testing_model.py
+++ testing_model.py
@@ -3,13 +3,16 @@ import gym
 import math
 from TurtleBotGym import TurtleBot
 from stable_baselines3 import DQN
+from stable_baselines3 import PPO
 import matplotlib.pyplot as plt
 
 # env = TurtleBot(1)
-env = gym.make("CartPole-v1")
+# env = gym.make("CartPole-v1")
+env = gym.make("MountainCar-v0")
 observation = env.reset()
 
-model = DQN.load("dqn_cartpole", env=env)
+# model = DQN.load("dqn_cartpole", env=env)
+model = PPO.load("ppo_mountaincar", env=env)
 # print(observation)
 for i in range(0,500):
     # action = env.action_space.sample()
